48e8f6812197f4f1fcccfeeb4c0b6b83df63c86d
JULFY <bkonarivskij@gmail.com>
2024-11-20 20:37:15 +0200

    ff

--- ConsoleApplication12.cpp
+++ ConsoleApplication12.cpp
@@ -14,21 +14,21 @@ public:
     Car(const string& brand, int maxSpeed)
         : brand(brand), maxSpeed(maxSpeed), Speed(0) {}
 
-    virtual void start() const override {
+    virtual void start() const{
         cout << "Car " << brand << " start" << endl;
     }
 
-    virtual void stop() const override {
+    virtual void stop() const{
         cout << "Car " << brand << " stop" << endl;
     }
 
-    virtual void upSpeed(int addsp) override {
+    virtual void upSpeed(int addsp){
         Speed += addsp;
         if (Speed > maxSpeed) Speed = maxSpeed;
         cout << "Car " << brand << " speed increased to " << Speed << endl;
     }
 
-    virtual void downSpeed(int decsp) override {
+    virtual void downSpeed(int decsp){
         Speed -= decsp;
         if (Speed < 0) Speed = 0;
         cout << "Car " << brand << " speed decreased to " << Speed  << endl;
@@ -45,20 +45,20 @@ public:
     Horse(const string& name, const string& breed)
         : name(name), breed(breed), speed(0) {}
 
-    virtual void start() const override {
+    virtual void start() const{
         cout << "Horse " << name << " start" << endl;
     }
 
-    virtual void stop() const override {
+    virtual void stop() const{
         cout << "Horse " << name << " stop" << endl;
     }
 
-    virtual void upSpeed(int addsp) override {
+    virtual void upSpeed(int addsp){
         speed += addsp;
         cout << "Horse " << name << " speed increased to " << speed << endl;
     }
 
-    virtual void downSpeed(int decsp) override {
+    virtual void downSpeed(int decsp){
         speed -= decsp;
         if (speed < 0) speed = 0;
         cout << "Horse " << name << " speed decreased to " << speed  << endl;
